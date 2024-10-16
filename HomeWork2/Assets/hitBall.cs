38b3aa1be669428263e1ce9c6f145c6a803f743c
Hutan Stefan <61848535+hutanstefan@users.noreply.github.com>
2024-10-16 13:42:55 +0300

    sound+effects

--- HomeWork2/Assets/hitBall.cs
+++ HomeWork2/Assets/hitBall.cs
@@ -4,32 +4,63 @@ using UnityEngine;
 
 public class hitBall : MonoBehaviour
 {
-    public float hitForce = 10f;
-    public float friction = 0.98f;
+    public float hitForce = 1000f;
+    public AudioSource hitSound; 
+    public GameObject ballEffect; 
+    public float stopThreshold = 0.1f; 
 
-    public Vector3 hitDirection = new Vector3(1, 0, 0); 
+    private Rigidbody rb;
+    private bool isMoving = false;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        if (ballEffect != null)
+        {
+            ballEffect.SetActive(false); 
+        }
     }
 
     void Update()
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
-        if (rb.velocity.magnitude > 0.1f)
+         if (isMoving)
         {
-            rb.velocity *= friction;
-        }     
+            if (ballEffect != null)
+            {
+                ballEffect.transform.position = transform.position;
+            }
+
+            if (rb.velocity.magnitude < stopThreshold)
+            {
+                isMoving = false;
+                if (ballEffect != null)
+                {
+                    ballEffect.SetActive(false);
+                }
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Club"))
         {
-            Vector3 impactDirection = collision.contacts[0].normal;
-            
-            GetComponent<Rigidbody>().AddForce(-impactDirection * hitForce, ForceMode.Impulse);
+            Vector3 impactDirection = (transform.position - collision.transform.position).normalized;
+            impactDirection.z = 0;
+
+            rb.AddForce(impactDirection * hitForce, ForceMode.Impulse);
+
+            if (hitSound != null)
+            {
+                hitSound.Play();
+            }
+
+            if (ballEffect != null)
+            {
+                ballEffect.SetActive(true);
+            }
+
+            isMoving = true;
         }
     }
 }
